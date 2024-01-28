f2fdd9d9f29eb0b36c28e1342fb5ba1660357932
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-28 12:59:37 +0600

    added shadcn and rr

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "./components/layouts/MainLayout";
+
+function App() {
+  return (
+    <>
+      <div>
+        <MainLayout />
+      </div>
+    </>
+  );
+}
+
+export default App;
